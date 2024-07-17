46f8f0bf34b43df19d6daa2a63d8221dd2ef2e37
Hugo Suarez <hugo.suarez92@yahoo.com>
2024-07-17 14:10:12 -0400

    create relationships in models

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Conversation extends Model
 {
@@ -14,4 +15,19 @@ class Conversation extends Model
         'user_id2',
         'last_message_id'
     ];
+
+    public function lastMessage(): BelongsTo
+    {
+        return $this->belongsTo(Message::class, 'last_message_id');
+    }
+
+    public function user1() : BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id1');
+    }
+
+    public function user2() : BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id2');
+    }
 }
