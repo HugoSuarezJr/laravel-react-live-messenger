529ab8f4a8db9c53cbb6791b89ae80456c9831f3
Hugo Suarez <hugo.suarez92@yahoo.com>
2024-07-18 17:27:37 -0400

    create database seeder and migrate/seed db

--- database/migrations/2024_07_16_203152_create_groups_table.php
+++ database/migrations/2024_07_16_203152_create_groups_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
         Schema::create('group_users', function (Blueprint $table) {
             $table->id();
             $table->foreignId('group_id')->constrained('groups')->onDelete('cascade');
-            $table->foreign('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
